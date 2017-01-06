72fc380bb003af9c07ea26f61b4e925885a73c33
Andrew <adf@ncgr.org>
2017-01-06 14:39:45 -0700

    first proof-of-principle linking into CoGe. Fairly crude, but "does the needful"

--- genome-context-viewer/app/components/shared/gene-detail.component.ts
+++ genome-context-viewer/app/components/shared/gene-detail.component.ts
@@ -7,6 +7,7 @@ import { Component,
 // App
 import { DetailsService } from '../../services/details.service';
 import { Gene }           from '../../models/gene.model';
+import { MicroTracks } from '../../models/micro-tracks.model';
 
 @Component({
   moduleId: module.id,
@@ -16,6 +17,7 @@ import { Gene }           from '../../models/gene.model';
     <h4>{{gene.name}}</h4>
     <p>Family: <a href="http://legumeinfo.org/chado_gene_phylotree_v2?family={{gene.family}}&gene_name={{gene.name}}">{{gene.family}}</a></p>
     <p><a href="#/search/{{gene.source}}/{{gene.name}}">Search for similar contexts</a></p>
+    <p><a *ngIf="alignedQueryGene" href="http://genomevolution.org/CoGe/GEvo.pl?accn1={{alignedQueryGene.name}};dr1up=50000;dr1down=50000;accn2={{gene.name}};dr2up=50000;dr2down=50000;num_seqs=2">Align region surrounding {{gene.name}} and {{alignedQueryGene.name}} in CoGe</a></p>
     <ul>
       <li *ngFor="let link of links">
         <a href="{{link.href}}">{{link.text}}</a>
@@ -27,8 +29,10 @@ import { Gene }           from '../../models/gene.model';
 
 export class GeneDetailComponent implements OnChanges {
   @Input() gene: Gene;
+  @Input() tracks: MicroTracks;
 
   links: any[];
+  alignedQueryGene: Gene;
 
   constructor(private _detailsService: DetailsService) { }
 
@@ -39,6 +43,14 @@ export class GeneDetailComponent implements OnChanges {
       this._detailsService.getGeneDetails(this.gene, links => {
         this.links = links;
       });
+
+
+      if (this.tracks !== undefined) {
+        var queryGenes = this.tracks.groups[0].genes;       
+        var alignmentColumn = this.gene.x;
+        this.alignedQueryGene = queryGenes.find(function(g){return g.x==alignmentColumn;});
+      }
+
     }
   }
 }

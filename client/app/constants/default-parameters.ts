26006394194d7e9f011db33860e1d119bb74452d
Mr. Legumoto <mr_legumoto@comparative-legumes.org>
2017-06-23 16:02:07 -0600

    cicre-demo config

--- client/app/constants/default-parameters.ts
+++ client/app/constants/default-parameters.ts
@@ -2,10 +2,10 @@
 export const DEFAULT_NEIGHBORS = 10;
 export const DEFAULT_MATCHED = 4;
 export const DEFAULT_INTERMEDIATE = 5;
-export const DEFAULT_SOURCE = 'lis';
+export const DEFAULT_SOURCE = 'cicre';
 
 // alignment parameters
-export const DEFAULT_ALIGNMENT = 'repeat'
+export const DEFAULT_ALIGNMENT = 'smith'
 export const DEFAULT_MATCH = 10;
 export const DEFAULT_MISMATCH = -1;
 export const DEFAULT_GAP = -1;

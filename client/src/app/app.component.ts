0ebb857ec655a9b1a1a8629cac3f726d5b8fd234
Andrew Farmer <adf@ncgr.org>
2017-10-21 21:05:27 -0500

    clarified the comment about the tours inclusion strategy

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
-//adf: this is ultra-temporary hack, just to see if I can make it work. If 
-//it works this way then maybe consider using a git submodule for the lis_tours
+//adf: following some discussion with acleary, we decided this is probably the
+//best approach to enabling the tours for the legumeinfo-settings branch. 
+//We may in future consider using a git submodule for the lis_tours
 //or else see if upgrading the webpack on lis_tours allows it to be handled as
-//external
+//external, in order to avoid this path dependency on the location of the
+//tours on the lis system
 import '/usr/local/www/drupal7/sites/all/modules/lis_tours/build/bundle.js';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../assets/css/styles.css';

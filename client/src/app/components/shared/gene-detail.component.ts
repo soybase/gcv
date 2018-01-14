7e83e405745cbd42180d3ccb6622e699247e999e
adf-ncgr <adf@ncgr.org>
2018-01-13 18:52:53 -0700

    get the chromosome for the clicked gene by digging through the groups for one containing the gene

--- client/src/app/components/shared/gene-detail.component.ts
+++ client/src/app/components/shared/gene-detail.component.ts
@@ -7,6 +7,8 @@ import { Component,
 // App
 import { DetailsService } from '../../services/details.service';
 import { Gene }           from '../../models/gene.model';
+import { MicroTracks }    from '../../models/micro-tracks.model';
+import { Group }    from '../../models/group.model';
 
 @Component({
   moduleId: module.id.toString(),
@@ -14,6 +16,7 @@ import { Gene }           from '../../models/gene.model';
   template: `
     <spinner [data]="links"></spinner>
     <h4>{{gene.name}}</h4>
+    <p><a href="http://localhost:60151/goto?locus={{group.chromosome_name.substring(group.chromosome_name.indexOf('.')+1)}}:{{gene.fmin-500}}-{{gene.fmax+500}} {{tracks.groups[0].chromosome_name.substring(tracks.groups[0].chromosome_name.indexOf('.')+1)}}:{{alignedQueryGene.fmin-500}}-{{alignedQueryGene.fmax+500}}">View {{gene.name}} and {{alignedQueryGene.name}} in IGV</a></p>
     <p>Family: <a href="http://legumeinfo.org/chado_gene_phylotree_v2?family={{gene.family}}&gene_name={{gene.name}}">{{gene.family}}</a></p>
     <p><a href="#/search/{{gene.source}}/{{gene.name}}">Search for similar contexts</a></p>
     <ul>
@@ -27,8 +30,11 @@ import { Gene }           from '../../models/gene.model';
 
 export class GeneDetailComponent implements OnChanges {
   @Input() gene: Gene;
+  @Input() group: Group;
+  @Input() tracks: MicroTracks;
 
   links: any[];
+  alignedQueryGene: Gene;
 
   constructor(private _detailsService: DetailsService) { }
 
@@ -39,6 +45,21 @@ export class GeneDetailComponent implements OnChanges {
       this._detailsService.getGeneDetails(this.gene, links => {
         this.links = links;
       });
+    }
+    if (this.tracks !== undefined) {
+        var queryGenes = this.tracks.groups[0].genes;
+        var alignmentColumn = this.gene.x;
+        this.alignedQueryGene = queryGenes.find(function(g){return g.x==alignmentColumn;});
+        if (this.gene.name == this.alignedQueryGene.name) {
+            this.alignedQueryGene = undefined;
+        }
+        else {
+            var gene = this.gene;
+            this.group = this.tracks.groups.find(
+                function(grp){return grp.genes.some(
+                    function(g) {
+                        return g.name === gene.name;});});
+        }
     }
   }
 }

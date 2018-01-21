53b7d7486b912022e9739b0349eaaa13f84a7e46
adf-ncgr <adf@ncgr.org>
2018-01-21 16:56:48 -0700

    fix logic for case where clicked gene is not actually aligned (and is not in the query track)

--- client/src/app/components/shared/gene-detail.component.ts
+++ client/src/app/components/shared/gene-detail.component.ts
@@ -17,9 +17,9 @@ import { Group }    from '../../models/group.model';
     <spinner [data]="links"></spinner>
     <h4>{{gene.name}}</h4>
     <!-- FIXME: the substring is to strip off prefix in context of peanut annotation evaluation. should remove it later-->
-    <p *ngIf="alignedQueryGene"><a href="http://localhost:60151/goto?locus={{group.chromosome_name.substring(group.chromosome_name.indexOf('.')+1)}}:{{gene.fmin-500}}-{{gene.fmax+500}} {{tracks.groups[0].chromosome_name.substring(tracks.groups[0].chromosome_name.indexOf('.')+1)}}:{{alignedQueryGene.fmin-500}}-{{alignedQueryGene.fmax+500}}">View {{gene.name}} and {{alignedQueryGene.name}} in IGV</a></p>
+    <p *ngIf="alignedQueryGene && tracks && group"><a href="http://localhost:60151/goto?locus={{group.chromosome_name.substring(group.chromosome_name.indexOf('.')+1)}}:{{gene.fmin-500}}-{{gene.fmax+500}} {{tracks.groups[0].chromosome_name.substring(tracks.groups[0].chromosome_name.indexOf('.')+1)}}:{{alignedQueryGene.fmin-500}}-{{alignedQueryGene.fmax+500}}">View {{gene.name}} and {{alignedQueryGene.name}} in IGV</a></p>
     <!--FIXME: seems like the version of angular we're using doesn't support else, but newer versions do-->
-    <p *ngIf="! alignedQueryGene"><a href="http://localhost:60151/goto?locus={{group.chromosome_name.substring(group.chromosome_name.indexOf('.')+1)}}:{{gene.fmin-500}}-{{gene.fmax+500}}">View {{gene.name}} in IGV</a>
+    <p *ngIf="(! alignedQueryGene) && tracks && group"><a href="http://localhost:60151/goto?locus={{group.chromosome_name.substring(group.chromosome_name.indexOf('.')+1)}}:{{gene.fmin-500}}-{{gene.fmax+500}}">View {{gene.name}} in IGV</a>
     </p>
     <p>Family: <a href="http://legumeinfo.org/chado_gene_phylotree_v2?family={{gene.family}}&gene_name={{gene.name}}">{{gene.family}}</a></p>
     <p><a href="#/search/{{gene.source}}/{{gene.name}}">Search for similar contexts</a></p>
@@ -45,26 +45,25 @@ export class GeneDetailComponent implements OnChanges {
   ngOnChanges(changes: SimpleChanges): void {
     this.links = undefined;
     if (this.gene !== undefined) {
-      this.links = undefined;
       this._detailsService.getGeneDetails(this.gene, links => {
         this.links = links;
       });
-    }
-    if (this.tracks !== undefined) {
-        var queryGenes = this.tracks.groups[0].genes;
-        var alignmentColumn = this.gene.x;
-        this.alignedQueryGene = queryGenes.find(function(g){return g.x==alignmentColumn;});
-        if (this.gene.name == this.alignedQueryGene.name) {
-            this.alignedQueryGene = undefined;
-            this.group = this.tracks.groups[0];
-        }
-        else {
-            var gene = this.gene;
-            this.group = this.tracks.groups.find(
-                function(grp){return grp.genes.some(
-                    function(g) {
-                        return g.name === gene.name;});});
-        }
+      if (this.tracks !== undefined) {
+          var queryGenes = this.tracks.groups[0].genes;
+          var alignmentColumn = this.gene.x;
+          this.alignedQueryGene = queryGenes.find(function(g){return g.x==alignmentColumn;});
+          if (this.alignedQueryGene && this.gene.name == this.alignedQueryGene.name) {
+              this.alignedQueryGene = undefined;
+              this.group = this.tracks.groups[0];
+          }
+          else {
+              var gene = this.gene;
+              this.group = this.tracks.groups.find(
+                  function(grp){return grp.genes.some(
+                      function(g) {
+                          return g.name === gene.name;});});
+          }
+      }
     }
   }
 }

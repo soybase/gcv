777cf7db2aa1983b5eb0996af9876b173c364703
Andrew Farmer <adf@ncgr.org>
2018-02-26 16:49:50 -0600

    added gene list -> legumemine option for tracklist component (to support a use case)

--- client/src/app/components/shared/track-detail.component.ts
+++ client/src/app/components/shared/track-detail.component.ts
@@ -13,6 +13,7 @@ import { Group }          from '../../models/group.model';
   template: `
     <h4>{{track.genus[0]}}.{{track.species}} - {{track.chromosome_name}}</h4>
     <p><a href="#/search/{{track.source}}/{{focus}}">Search for similar contexts</a></p>
+    <p ><a href="https://intermine.legumefederation.org/legumemine/bag.do?type=Gene&text={{geneListFormatted}}">Create gene list in LegumeMine</a></p>
     <p>Genes:</p>
     <ul>
       <li *ngFor="let gene of track.genes">
@@ -32,11 +33,13 @@ export class TrackDetailComponent implements OnChanges {
   @Input() track: Group;
 
   focus: string;
+  geneListFormatted: string;
 
   ngOnChanges(changes: SimpleChanges): void {
     if (this.track !== undefined) {
       let idx = Math.floor(this.track.genes.length / 2);
       this.focus = this.track.genes[idx].name;
+      this.geneListFormatted = this.track.genes.map(x => x.name).join('%0A');
     }
   }
 }

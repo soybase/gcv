215ce298119170a2a8cf23b9991bad2121bb5c19
Andrew Farmer <adf@ncgr.org>
2018-03-10 20:41:36 -0600

    some awful hackery to allow gene lists to be made against ThaleMine PhytoMine. Should make this functionality use the linkout service (encapuslating the implementation hackery behind the interface elegance)

--- client/src/app/components/shared/track-detail.component.ts
+++ client/src/app/components/shared/track-detail.component.ts
@@ -13,6 +13,10 @@ import { Group }          from '../../models/group.model';
   template: `
     <h4>{{track.genus[0]}}.{{track.species}} - {{track.chromosome_name}}</h4>
     <p><a href="#/search/{{track.source}}/{{focus}}">Search for similar contexts</a></p>
+    <!-- FIXME: AWFUL HACKS FOLLOW -->
+    <p *ngIf="track.genus != 'Arabidopsis' && track.genus != 'Cucumis' && track.genus != 'Fragaria' && track.genus != 'Prunus'"><a href="https://intermine.legumefederation.org/legumemine/bag.do?type=Gene&text={{geneListFormatted}}">Create gene list in LegumeMine</a></p>
+    <p *ngIf="track.genus === 'Arabidopsis'"><a href="https://apps.araport.org/thalemine/bag.do?type=Gene&text={{geneListFormatted}}">Create gene list in ThaleMine</a></p>
+    <p *ngIf="track.genus === 'Cucumis' || track.genus === 'Fragaria' || track.genus === 'Prunus'"><a href="https://phytozome.jgi.doe.gov/phytomine/bag.do?type=Gene&text={{geneListFormatted}}">Create gene list in PhytoMine</a></p>
     <p>Genes:</p>
     <ul>
       <li *ngFor="let gene of track.genes">
@@ -32,11 +36,15 @@ export class TrackDetailComponent implements OnChanges {
   @Input() track: Group;
 
   focus: string;
+  geneListFormatted: string;
 
   ngOnChanges(changes: SimpleChanges): void {
     if (this.track !== undefined) {
       let idx = Math.floor(this.track.genes.length / 2);
       this.focus = this.track.genes[idx].name;
+      this.geneListFormatted = this.track.genes.map(x => x.name).join('%0A');
+      //ANOTHER TERRIBLE HACK!
+      this.geneListFormatted = this.geneListFormatted.replace(/arath.Col./g,'')
     }
   }
 }

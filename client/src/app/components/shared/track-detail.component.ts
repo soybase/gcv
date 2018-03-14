bc7a2b3a398b28c2588a715f7922079e8eee27d3
Andrew Farmer <adf@ncgr.org>
2018-03-13 23:05:50 -0500

    add subtab=upload param to mine list creation

--- client/src/app/components/shared/track-detail.component.ts
+++ client/src/app/components/shared/track-detail.component.ts
@@ -14,9 +14,9 @@ import { Group }          from '../../models/group.model';
     <h4>{{track.genus[0]}}.{{track.species}} - {{track.chromosome_name}}</h4>
     <p><a href="#/search/{{track.source}}/{{focus}}">Search for similar contexts</a></p>
     <!-- FIXME: AWFUL HACKS FOLLOW -->
-    <p *ngIf="track.genus != 'Arabidopsis' && track.genus != 'Cucumis' && track.genus != 'Fragaria' && track.genus != 'Prunus'"><a href="https://intermine.legumefederation.org/legumemine/bag.do?type=Gene&text={{geneListFormatted}}">Create gene list in LegumeMine</a></p>
-    <p *ngIf="track.genus === 'Arabidopsis'"><a href="https://apps.araport.org/thalemine/bag.do?type=Gene&text={{geneListFormatted}}">Create gene list in ThaleMine</a></p>
-    <p *ngIf="track.genus === 'Cucumis' || track.genus === 'Fragaria' || track.genus === 'Prunus'"><a href="https://phytozome.jgi.doe.gov/phytomine/bag.do?type=Gene&text={{geneListFormatted}}">Create gene list in PhytoMine</a></p>
+    <p *ngIf="track.genus != 'Arabidopsis' && track.genus != 'Cucumis' && track.genus != 'Fragaria' && track.genus != 'Prunus'"><a href="https://intermine.legumefederation.org/legumemine/bag.do?subtab=upload&type=Gene&text={{geneListFormatted}}">Create gene list in LegumeMine</a></p>
+    <p *ngIf="track.genus === 'Arabidopsis'"><a href="https://apps.araport.org/thalemine/bag.do?subtab=upload&type=Gene&text={{geneListFormatted}}">Create gene list in ThaleMine</a></p>
+    <p *ngIf="track.genus === 'Cucumis' || track.genus === 'Fragaria' || track.genus === 'Prunus'"><a href="https://phytozome.jgi.doe.gov/phytomine/bag.do?subtab=upload&type=Gene&text={{geneListFormatted}}">Create gene list in PhytoMine</a></p>
     <p>Genes:</p>
     <ul>
       <li *ngFor="let gene of track.genes">
